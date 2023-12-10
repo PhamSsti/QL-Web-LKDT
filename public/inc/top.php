62d1771e2df88f7349025181156ccfca5cc5392a
PhamSsti <118352015+PhamSsti@users.noreply.github.com>
2023-12-10 11:30:22 +0700

    update top

--- public/inc/top.php
+++ public/inc/top.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
-        <title>Văn phòng phẩm AGU</title>
+        <title>Shop 3KLN</title>
 
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
@@ -16,7 +16,7 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-info shadow">
             <div class="container px-4 px-lg-5">
-                <a class="navbar-brand" href="index.php"><i class="bi bi-shop-window"></i> ABC Shop</a>
+                <a class="navbar-brand" href="index.php"><i class="bi bi-shop-window"></i> 3KLN Shop</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
